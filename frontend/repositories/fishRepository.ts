c432cd38976314370e27a9d062aa8fa96180b8e6
Sakai-Kaede <kana1024k@gmail.com>
2025-02-16 18:01:49 +0900

    RepositoryFactoryパターンの改善

--- frontend/repositories/fishRepository.ts
+++ frontend/repositories/fishRepository.ts
@@ -1,25 +1,21 @@
-const {
-  public: { apiBase },
-} = useRuntimeConfig();
-
 export default {
   async fetchPreFish(userId: string, depth: number) {
-    const response = await $fetch(`${apiBase}/get-fish/${userId}`, {
+    const apiBase = useNuxtApp().$apiBase;
+    return await $fetch(`${apiBase}/get-fish/${userId}`, {
       method: "POST",
       body: {
         depth,
       },
     });
-    return response;
   },
 
   async catchFish(userId: string, randomId: string) {
-    const response = await $fetch(`${apiBase}/catch-fish/${userId}`, {
+    const apiBase = useNuxtApp().$apiBase;
+    return await $fetch(`${apiBase}/catch-fish/${userId}`, {
       method: "POST",
       body: {
         randomId,
       },
     });
-    return response;
   },
 };

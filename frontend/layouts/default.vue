6cd89fdc1c5885b42935978e0e6631e1c2ea59ae
Sakai-Kaede <kana1024k@gmail.com>
2025-03-05 01:44:33 +0900

    ハンバーガーメニューの改善

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <AtomsHamburgerMenu class="hamburger-menu" />
+    <AtomsHamburgerMenu v-show="userStore.userId" class="hamburger-menu" />
     <AtomsUserProfile class="user-profile" />
     <NuxtPage />
   </div>
 </template>
 
+<script setup lang="ts">
+import { useUserStore } from "@/store/user";
+const userStore = useUserStore();
+</script>
+
 <style scoped lang="scss">
 .hamburger-menu {
   position: fixed;
-  top: 2rem;
-  left: 2rem;
+  top: 2.5rem;
+  left: 2.5rem;
   z-index: 1000;
 }
 .user-profile {
